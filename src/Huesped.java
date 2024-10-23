a7ad8c3438d23025f12d23b774b63b364e537355
Angel Arias <150385139+angelariasus@users.noreply.github.com>
2024-10-22 21:26:43 -0500

    Actualización
    + Herencia

--- src/Huesped.java
+++ src/Huesped.java
@@ -47,3 +47,64 @@ public class Huesped {
         return detalles;
     }
 }
+
+
+public class Huesped extends Persona {
+    private Reserva reserva;
+
+    public Huesped(String nombre, String tipoDocumento, int numeroDocumento, String pais) {
+        super(nombre, tipoDocumento, numeroDocumento, pais); 
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+
+    @Override
+    public String Detalles() {
+        String detalles = super.Detalles();  
+        if (reserva != null) {
+            detalles += " - Reserva: " + reserva;
+        }
+        return detalles;
+    }
+}
+
+
+public class Persona {
+    private final String nombre;
+    private final String tipoDocumento;
+    private final int numeroDocumento;
+    private final String pais;
+
+    public Persona(String nombre, String tipoDocumento, int numeroDocumento, String pais) {
+        this.nombre = nombre;
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public int getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String DetallesMinimos() {
+        return "Nombre: " + nombre + " - " + tipoDocumento + ": " + numeroDocumento + "\n";
+    }
+
+    public String Detalles() {
+        return "Nombre: " + nombre + " - " + tipoDocumento + " " + numeroDocumento + " - " + pais;
+    }
+}
